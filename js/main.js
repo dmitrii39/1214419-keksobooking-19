5a4b3a871ddf40bf5b1da027a43f421eb7f6d0de
dmitrii39 <d.tsyplov@mail.ru>
2020-03-06 00:31:11 +0200

    готово 06.03.2020

--- js/main.js
+++ js/main.js
@@ -1,367 +1,153 @@
 "use strict";
 
-
 // ---ИСХОДНЫЕ ДАННЫЕ------------------
 var user = [
-  "img/avatars/user01.png",
-  "img/avatars/user02.png",
-  "img/avatars/user03.png",
-  "img/avatars/user04.png",
-  "img/avatars/user05.png",
-  "img/avatars/user06.png",
-  "img/avatars/user07.png",
-  "img/avatars/user08.png"
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png'
 ];
 
-var offer = [
-  {
-    title: "Привет мой случайный гость",
-    address: "600, 350",
-    price: 100,
-    type: "palace",
-    rooms: 4,
-    guests: 8,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "бла-бла-бла",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  },
+var type = ['palace', 'flat', 'house', 'bungalo'];
 
-  {
-    title: "Привет мой случайный гость",
-    address: "500, 300",
-    price: 90,
-    type: "bungalo",
-    rooms: 2,
-    guests: 7,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "cozy apartmens",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  },
+var price = [100, 200, 300, 400, 500, 600, 700];
 
-  {
-    title: "Привет мой случайный гость",
-    address: "550, 250",
-    price: 55,
-    type: "house",
-    rooms: 3,
-    guests: 4,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "бла-бла-бла",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  },
+var checkin = ['12: 00', '13: 00', '14: 00'];
+var checkout = ['12: 00', '13: 00', '14: 00'];
 
-  {
-    title: "Привет мой случайный гость",
-    address: "100, 110",
-    price: 60,
-    type: "flat",
-    rooms: 5,
-    guests: 4,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "бла-бла-бла",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  },
 
-  {
-    title: "Привет мой случайный гость",
-    address: "300, 200",
-    price: 20,
-    type: "bungalo",
-    rooms: 222,
-    guests: 4,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "бла-бла-бла",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  },
+var features = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'];
 
-  {
-    title: "Привет мой случайный гость",
-    address: "200, 100",
-    price: 50,
-    type: "flat",
-    room: 2,
-    guests: 156,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "бла-бла-бла",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  },
-
-  {
-    title: "Привет мой случайный гость",
-    address: "280, 70",
-    price: 1,
-    type: "bungalo",
-    rooms: 0,
-    guests: 1,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "бла-бла-бла",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  },
-
-  {
-    title: "Привет мой случайный гость",
-    address: "580, 310",
-    price: 10,
-    type: "bungalo",
-    rooms: 2,
-    guests: 3,
-    checkin: "14: 00",
-    checkout: "12: 00",
-    feature: [
-      "wifi",
-      "dishwashe",
-      "parking",
-      "washer",
-      "elevator",
-      "conditioner"
-    ],
-    description: "бла-бла-бла",
-    photos: [
-      "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-      "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-    ]
-  }
+var photos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
 
+var checkinRandom = function () {
+  var checkinRandomIndex = Math.floor(Math.random() * checkin.length);
+  return checkin[checkinRandomIndex];
+}
+var checkoutRandom = function () {
+  var checkoutRandomIndex = Math.floor(Math.random() * checkout.length);
+  return checkout[checkoutRandomIndex];
+}
 
-var loc = [
+var fotosRandom = function () {
+  var fotosRandomIndex = Math.floor(Math.random() * photos.length)
+  return photos[fotosRandomIndex];
+}
 
-  {
-    x: 700,
-    y: 500
-  },
+var pointX = function () {
+  var pointXRandom = Math.floor(Math.random() * 900);
+  return pointXRandom
+};
 
-  {
-    x: 20,
-    y: 400
-  },
+var pointY = function () {
+  var pointYRandom = Math.floor(Math.random() * 500);
+  return pointYRandom
+};
 
-  {
-    x: 140,
-    y: 300
-  },
+var loc = { x: pointX(), y: pointY() };
 
-  {
-    x: 200,
-    y: 200
-  },
-
-  {
-    x: 800,
-    y: 620
-  },
-
-  {
-    x: 70,
-    y: 320
-  },
-
-  {
-    x: 300,
-    y: 275
-  },
-
-  {
-    x: 100,
-    y: 240
-  }
-
-];
 
 // --------------------КОНЕЦ ИСХОДНЫХ ДАННЫХ--------------------
 
-
-
-
 // -----------------РЕШЕНИЕ-------------------------
+
+var typeRandom = function () {
+  var typeRandomIndex = Math.floor(Math.random() * type.length);
+  return type[typeRandomIndex];
+};
+
 var userRandom = function () {
   var userRandomIndex = Math.floor(Math.random() * user.length);
   return user[userRandomIndex];
 
 };
-userRandom();
-
 
 var offerRandom = function () {
-  var offerRandomIndex = Math.floor(Math.random()* offer.length);
+  var offerRandomIndex = Math.floor(Math.random() * offer.length);
   return offer[offerRandomIndex];
 };
-offerRandom();
-
 
 var locRandom = function () {
-  var locRandomIndex = Math.floor(Math.random()* loc.length);
+  var locRandomIndex = Math.floor(Math.random() * loc.length);
   return loc[locRandomIndex];
-}
-locRandom();
-
-
-
-var createAuthors = function () {
-  var authors = [];
-  for (var i = 0; i < 8; i++) {
-    var bookings = {
-      users: userRandom(),
-      offers: offerRandom(),
-      locations: locRandom()
-    };
-    authors.push(bookings);
-  }
-  
-  return authors;
 };
 
-createAuthors();
+function getRandLenghtArr() {
+  var randIndex = Math.floor(Math.random() * features.length);
+  return features.slice(0, randIndex);
+};
 
+
+function offerGenerator(total) {
+  var offers = [];
+  for (var i = 0; i < total; i++) {
+    var offer = {
+     author: { avatar: userRandom(), },
+      offerList:
+      {
+        title: "Привет мой случайный гость",
+        address: "600, 350",
+        price: 55,
+        type: typeRandom(),
+        rooms: 3,
+        guests: 4,
+        checkin: checkinRandom(),
+        checkout: checkoutRandom(),
+        feature: getRandLenghtArr(),
+        description: "бла-бла-бла",
+        photos: fotosRandom()
+      },
+
+      location:
+      {
+        x: pointX(),
+        y: pointY()
+      }
+    };
+    offers.push(offer);
+
+  }
+
+  return offers;
+
+};
+offerGenerator(8);
 
 var removeMap = document.querySelector(".map");
 removeMap.classList.remove("map--faded");
 
-
-
-var allUsers= function() {
-   for (var i = 0; i<=8; i++){
-     var similarUserTemplate = document.querySelector('.map__pins');
-     var similarListElement = document.querySelector('.map__pin');
-
-     var userClone = similarListElement.cloneNode(true);
-     var newUser = similarUserTemplate.appendChild(userClone);
-     newUser.style = 'left: ' + loc[i].x + 'px; ' + 'top: ' + loc[i].y + 'px;';
-     var userAvatar = similarListElement.querySelector('img');
-     userAvatar.src = user[i];
-     userAvatar.alt = user[i];
-
-
-   }
-
+var allUsers = function(ttl) {
+  for (var i = 0; i < ttl; i++) {
+    var similarUserTemplate = document.querySelector(".map__pins");
+    var similarListElement = document.querySelector(".map__pin");  
+    var userClone = similarListElement.cloneNode(true);
+    var newUser = similarUserTemplate.appendChild(userClone);
+    newUser.style = "left: " + pointX() + "px; " + "top: " + pointY() + "px;";
+    var userAvatar = similarListElement.querySelector("img");
+    userAvatar.src = user[i];
+    userAvatar.alt = user[i];
+  }
 };
 
- allUsers();
+allUsers(8);
+
 
 //  ---------КОНЕЦ РЕШЕНИЯ----------------
-
-
-// --------------(НЕ ПОЛУЧАЕТСЯ) ВАРИАНТ ВЫВОДА ЧЕРЕЗ DocumentFragment----------
-
-
- //  var allUsers = function() {
-//    var similarUserTemplate = document.querySelector('.map__pins');
-//    var similarListElement = document.querySelector('.map__pin');
-//    var fragment = document.createDocumentFragment();
-//    for (var i = 0; i <= 8; i++) {
-
-//      var newUser = document.createElement('button');
-//      newUser.className = 'map__pin';
-//      newUser.style = 'left: ' + loc[i].x + 'px; ' + 'top: ' + loc[i].y + 'px;';
-//      var userAvatar = similarListElement.querySelector('img');
-//      userAvatar.src = user[i];
-
-//      fragment.appendChild(newUser);
-
-//    }
-//    similarUserTemplate.appendChild(fragment);
-
-//  };
- 
-
-
-
-
-
-
 
 
 
